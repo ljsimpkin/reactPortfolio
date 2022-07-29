afa264b8c89f20247cffdfe514f8b30cfd856968
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-07-30 01:58:46 +1200

    refactor data structure

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ljsimpkin.github.io/reactPortfolio.git main:gh-pages
+git push -f git@github.com:ljsimpkin/reactPortfolio.git main:gh-pages
 
 cd -
--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,12 +1,12 @@
 function Tile(props) {
-  const { title, group, description, url, imageSrc, src2 } = props.project
+  const { title, group, description, url, images } = props.project
   // const { setModel } = props.setModel
   // console.log(props.setModel)
   return (
     <div className={group} onClick={()=>props.setModel(props.project)}>
       <h1 className="masonry-title">{title}</h1>
       <h2 className="masonry-description">{description}</h2>
-      <img src={imageSrc}></img>
+      <img src={images[0]}></img>
     </div>
   )
 }
