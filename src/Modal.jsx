f7c6f8610d4e0507cb18eae31a6cfa62b439845d
liam <ljsimpkin@gmail.com>
2026-03-26 22:14:01 +1300

    imporved modal design for desktop

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -9,15 +9,19 @@ function Modal(props) {
 
   return (
     <div className="modal" onClick={()=>props.setModel(null)}>
-      <div className="modal-content">
-        <span onClick={()=>props.setModel(null)} className="close">&times;</span>
-        <h1>{title}</h1>
-        <h2>{subHeading}</h2>
-        {description && <NewlineText text={description}/>}
-        {url && <a href={url} target="_blank" className="project search-button">See more</a>}
-        {images.map(image => 
-          <img className="modalImage" src={image}></img>
-        )}
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+        <div className="modal-images">
+          {images.map((image, i) =>
+            <img key={i} src={image}></img>
+          )}
+        </div>
+        <div className="modal-text">
+          <span onClick={()=>props.setModel(null)} className="close">&times;</span>
+          <h1>{title}</h1>
+          <h2>{subHeading}</h2>
+          {description && <NewlineText text={description}/>}
+          {url && <a href={url} target="_blank" className="project search-button">See more</a>}
+        </div>
       </div>
     </div>
   )
