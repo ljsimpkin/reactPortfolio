fe34f9f11592d30244cfc579644741a18afb6fa2
liam <ljsimpkin@gmail.com>
2026-03-26 22:22:02 +1300

    suggest to the user to scroll down the page when there is more than one image

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,5 @@
 import Masonry from 'react-masonry-css'
+import { useRef, useState } from 'react'
 
 function NewlineText(props) {
   return <div className="modalDescription">{props.text.split('\n').map(str => <p>{str}</p>)}</div>;
@@ -6,14 +7,23 @@ function NewlineText(props) {
 
 function Modal(props) {
   const { title, group, subHeading, description, url, images } = props.project
+  const imagesRef = useRef(null)
+  const [chevronVisible, setChevronVisible] = useState(true)
+
+  function handleScroll() {
+    if (imagesRef.current.scrollTop > 30) setChevronVisible(false)
+  }
 
   return (
     <div className="modal" onClick={()=>props.setModel(null)}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <div className="modal-images">
+        <div className="modal-images" ref={imagesRef} onScroll={handleScroll}>
           {images.map((image, i) =>
             <img key={i} src={image}></img>
           )}
+          {images.length > 1 &&
+            <div className="scroll-hint" style={{opacity: chevronVisible ? 1 : 0}}>&#8964;</div>
+          }
         </div>
         <div className="modal-text">
           <span onClick={()=>props.setModel(null)} className="close">&times;</span>
